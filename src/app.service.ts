c3bbef7aa8979346037f77154d7668d929c023fe
damonholden <damonh003@gmail.com>
2023-08-13 15:49:20 +0100

    feat: send moon data from base endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
+import { Observable, firstValueFrom, catchError } from 'rxjs';
+import { AxiosResponse, AxiosError } from 'axios';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly httpService: HttpService,
+  ) {}
+  async getMoonData(): Promise<Observable<AxiosResponse<JSON>>> {
+    const location = 'BA333';
+    const key = this.configService.get<string>('Q_WEATHER_API_KEY');
+    const lang = 'en';
+    const date = '20230813';
+    const url = `https://devapi.qweather.com/v7/astronomy/moon?location=${location}&date=${date}&lang=${lang}&key=${key}`;
+
+    const { data } = await firstValueFrom(
+      this.httpService.get(url).pipe(
+        catchError((error: AxiosError) => {
+          console.log(error);
+          throw new Error(error.message);
+        }),
+      ),
+    );
+
+    return data;
   }
 }
