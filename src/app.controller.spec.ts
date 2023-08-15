536cbfc5f9f10eb7dae21ff79dcb07c6b34c816d
damonholden <damonh003@gmail.com>
2023-08-15 17:22:05 +0100

    test: add default tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { CacheModule } from '@nestjs/cache-manager';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot(), HttpModule, CacheModule.register()],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -14,9 +18,17 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  // describe('root', () => {
-  //   it('should return "Hello World!"', () => {
-  //     expect(appController.getHello()).toBe('Hello World!');
-  //   });
-  // });
+  describe('root', () => {
+    it('should exist', () => {
+      expect(appController.root).toBeDefined();
+    });
+
+    it('should return string html of li elements each containing moon data', async () => {
+      const appControllerRes = await appController.root();
+
+      expect(appControllerRes.moonData).toMatch(
+        /<li>phase name: [\s\S]+<\/li><li>stage: [\s\S]+<\/li><li>days until next full moon: [\s\S]+<\/li><li>days until next new moon: [\s\S]+<\/li>/,
+      );
+    });
+  });
 });
